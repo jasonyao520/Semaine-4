d79a48bfd3786a3f28711515172cdda20a519231
jasonyao520 <yaos07834@gmail.com>
2025-07-10 04:06:04 +0200

    je viens d'ajoute les fichier 3 et 4  de mon exo

---
+++ exo_03.php
@@ -0,0 +1,10 @@
+<?php
+$my_array =[
+    "integer" => 42,//ici integer est la valeur et 42 est la cle
+    "float" => 42.42,
+    "string" => "Glory",
+    "bool" => true,//"si j'appelle bool sa affiche 1
+    "null" => null,//si j'appelle "null" rien ne va s'afficher 
+];
+echo $my_array["string"];
+?>
---
+++ exo_04.php
@@ -0,0 +1,6 @@
+<?php
+function my_concat($a , $b){
+    echo  $a ."   ".$b;
+}
+my_concat(5,15)
+?>
